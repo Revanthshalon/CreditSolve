a13229cc0e12562360c06e1f9d6d611bf8100f65
Revanthshalon <revanthshalon@gmail.com>
2023-11-13 11:07:17 +0530

    Added DB Context

--- src/data/dbContext.ts
+++ src/data/dbContext.ts
@@ -1,3 +1,11 @@
-const realmContext = [];
+import { createRealmContext } from "@realm/react";
+import Company from "../models/Company";
+import Purchase from "../models/Purchase";
+import Payment from "../models/Payment";
+
+const realmContext = createRealmContext({
+  schema: [Company, Purchase, Payment],
+  schemaVersion: 0,
+});
 
 export default realmContext;
