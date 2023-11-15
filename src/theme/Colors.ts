627d694ea68e6936e5dd9a720867373bf424749b
Revanthshalon <revanthshalon@gmail.com>
2023-11-15 10:36:57 +0530

    Added theme Colors

---
+++ src/theme/Colors.ts
@@ -0,0 +1,10 @@
+const darkTheme = {
+  dark: true,
+  colors: {
+    primary: "#555b6eff",
+    background: "#faf9f9ff",
+    border: "#89b0aeff",
+    card: "#bee3dbff",
+    notification: "#ffd6baff",
+  },
+};
